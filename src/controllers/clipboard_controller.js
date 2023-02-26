4b77601494d1788b8e1c8f00093ada031833c8b6
VolNa <grizli887@mail.ru>
2023-02-26 18:56:11 +0300

    first stimulus test

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+	static targets = ["source"];
+    static classes = [ "supported" ]
+	connect() {
+    if ("clipboard" in navigator) {
+      this.element.classList.add(this.supportedClass);
+    }
+  }
+}
